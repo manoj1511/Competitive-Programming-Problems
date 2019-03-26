b41c96e9c87d0c1ba3800ab8ddfd8f5d58d794fa
manoj1511 <csmanojkumar1995@gmail.com>
2019-03-26 17:41:13 -0500

    read number of test cases from arguments

--- Finding_index_in_sorted_array/main.cpp
+++ Finding_index_in_sorted_array/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		cout << "Please enter number of test cases" << endl;
+		cout << "USAGE : ./executable <no. of test case>" << endl;
+		return 1;
+	}
+	
+	int tc = atoi(argv[1]);
+	cout << tc << endl;
+	return 0;
+}
 
